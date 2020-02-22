d55ed1c27a39bbeb9386b57a0a64d34ee2e96ea5
purdonkle <adampurdondev@gmail.com>
2020-02-23 00:17:21 +0200

    fixed output for null input and added 3 more test cases

--- Problems.Tests/UnitTest1.cs
+++ Problems.Tests/UnitTest1.cs
@@ -10,25 +10,16 @@ namespace Problems.Tests
         {
         }
 
-      	[Test]
-        public void test8i()
+      	[TestCase("hello", "5,1:hello")]
+		[TestCase("", "0,0:")]
+		[TestCase("what    ...  did you say?? ", "27,5:what    ...  did you say?? ")]
+		[TestCase("This is a six worded sentance", "29,6:This is a six worded sentance")]
+		[TestCase("   hello    adam     ", "21,2:   hello    adam     ")]
+		[TestCase(null, null)]
+        public void test8i(string input, string output)
         {
 			Program program = new Program();
-            Assert.AreEqual(program.Prefix("hello"), "5,1:hello");
+            Assert.AreEqual(program.Prefix(input), output);
         }
-		
-		[Test]	
-		public void test8ii()
-		{
-			Program program = new Program();
-			Assert.AreEqual(program.Prefix(""), "0,0:");
-		}
-
-		[Test]
-		public void test8iii()
-		{
-			Program program = new Program();
-			Assert.AreEqual(program.Prefix("what    ...  did you say?? "), "27,5:what    ...  did you say?? ");
-		}
     }
 }
--- Problems/Program.cs
+++ Problems/Program.cs
@@ -13,6 +13,11 @@ namespace Problems
 
 		public string Prefix(string input)
 		{
+			if (input == null)
+			{
+				return null;
+			}			
+
 			int lengthOfString = input.Length;
 			int numberOfWords = input.Split(new char [0], StringSplitOptions.RemoveEmptyEntries).Length;
 		
