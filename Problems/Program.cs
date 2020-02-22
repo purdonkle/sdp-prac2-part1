b60063bb71c4161e4992f68f645f0dc2a27daf7e
purdonkle <adampurdondev@gmail.com>
2020-02-22 20:01:40 +0200

    added Prefix() and nunit template

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,21 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			Console.WriteLine(Prefix(args[0]));
         }
+
+		static string Prefix(string input)
+		{
+			int lengthOfString = input.Length;
+			int numberOfWords = input.Split(new char [0], StringSplitOptions.RemoveEmptyEntries).Length;
+		
+			string output = lengthOfString + "," + numberOfWords + ":" + input;
+						
+			return output;
+		}
     }
 }
